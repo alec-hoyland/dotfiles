4ad6944759d5cefb4f3ca5a0cf81000708c84122
Alec Hoyland <alec.hoyland@posteo.net>
2021-03-05 15:50:02 -0500

    minor fixes

--- install.sh
+++ install.sh
@@ -18,5 +18,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# source the correct files
+source ~/.zprofile
+source ~/.zshrc
+tmux source ~/.tmux.conf
+
 # copy dotfiles
 cp work/* ~/
