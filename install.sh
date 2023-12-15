707b45248655c41ac7ffe1cacf1854ef8ccf045f
Alec Hoyland <alec.hoyland@posteo.net>
2023-12-15 15:25:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,10 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
 # change shell to zsh
 chsh -s $(which zsh)
 
